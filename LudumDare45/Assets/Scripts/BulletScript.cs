c52c7830811cd5a325c4cc1105fc82b3d67d24e5
SandraBenard <52406865+SandraBenard@users.noreply.github.com>
2019-10-05 18:17:07 +0200

    SBD-SceneArena-BulletControl

---
+++ LudumDare45/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    //public GameObject hitEffect;
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f); //remove the effect particule or sprite after 5sec
+        Destroy(gameObject);
+    }*/
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet") )
+        {
+            Destroy(gameObject);
+        }
+        if (collision.CompareTag("EnemyCac"))
+        {
+            collision.GetComponent<EnemyControlCac>().SetHealth(10);
+            Destroy(gameObject);
+        }
+        if (collision.CompareTag("EnemyDistance"))
+        {
+            collision.GetComponent<EnemyControlDistance>().SetHealth(10);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LudumDare45/Assets/Scripts/EnemyControlCac.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControlCac : MonoBehaviour
+{
+    int health = 150;
+    float speed;
+    public Transform target;
+    public float attackRadius;
+    public float chaseRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    public void SetHealth(int nb)
+    {
+            health = health - nb;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void CheckDistance()
+    {
+        
+        if (Vector2.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
