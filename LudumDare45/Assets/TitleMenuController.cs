e46a75d286ef0411f700e0653dfef461f60040df
talesofpixelia <42294009+talesofpixelia@users.noreply.github.com>
2019-10-06 12:59:37 +0200

    [PPE] Title Screen Fixed

---
+++ LudumDare45/Assets/TitleMenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleMenuController : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip audioClip;
+
+    public Animator animator;
+
+    private bool exitrequired;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            audioSource.Play();
+
+            animator.SetTrigger("FadeOut");
+            exitrequired = true;
+        }
+
+        Debug.Log(string.Format("Normalized Time {0}, Is In Transition : {1}, Exit Required {2}", animator.GetCurrentAnimatorStateInfo(0).normalizedTime, !animator.IsInTransition(0), exitrequired));
+
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 2 && !animator.IsInTransition(0) && exitrequired)
+        {
+            StartCoroutine(StartGame());
+        }
+    }
+
+    public IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(1.8f);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
