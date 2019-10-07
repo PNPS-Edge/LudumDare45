dc8bcd5030f402221de990a7f7bfb9ffd6abdd06
talesofpixelia <42294009+talesofpixelia@users.noreply.github.com>
2019-10-07 17:16:41 +0200

    [PPE] End Screen finish

---
+++ LudumDare45/Assets/EndScreenController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenController : MonoBehaviour
+{
+    private bool exitrequired;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            FadeOut();
+        }
+    }
+
+    public void FadeOut()
+    {
+        animator.SetTrigger("FadeOut");
+        exitrequired = true;
+
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 2 && !animator.IsInTransition(0) && exitrequired)
+        {
+            StartCoroutine(ChangeScene());
+        }
+    }
+
+    public IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(1.8f);
+
+        SceneManager.LoadScene(0);
+    }
+}
