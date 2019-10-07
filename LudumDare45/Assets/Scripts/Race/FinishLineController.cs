63f2e713db4974a86f02f19ae93dde46ddba99a0
Valérian Perez <valerian.perez@gmail.com>
2019-10-07 20:30:22 +0200

    Add Finish "cinematic" & change value of Race scene

--- LudumDare45/Assets/Scripts/Race/FinishLineController.cs
+++ LudumDare45/Assets/Scripts/Race/FinishLineController.cs
@@ -5,31 +5,63 @@ using UnityEngine.SceneManagement;
 
 public class FinishLineController : MonoBehaviour
 {
+    #region Properties
     public Animator animator;
 
+    /// <summary>
+    /// Gets or sets the GO to move after the finish line
+    /// </summary>
+    public GameObject Exit;
+    #endregion
+
+    #region Fields
     private bool exitrequired;
+
+    /// <summary>
+    /// Ges or sets the player
+    /// </summary>
+    private GameObject player;
+    #endregion
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (this.exitrequired)
+        {
+            Vector3 pos = this.player.transform.position;
+            Vector3 newPos = Vector3.Lerp(pos, this.Exit.transform.position, Time.deltaTime * 5);
 
+            this.player.transform.position = newPos;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        animator.SetTrigger("FadeOut");
-        exitrequired = true;
-
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 2 && !animator.IsInTransition(0) && exitrequired)
+        if (collision.CompareTag("Player"))
         {
-            StartCoroutine(ChangeScene());
+            // Freeze the player 
+            collision.GetComponent<PlayerRaceController>().enabled = false;
+
+            animator.SetTrigger("FadeOut");
+            exitrequired = true;
+
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 2 && !animator.IsInTransition(0) && exitrequired)
+            {
+                StartCoroutine(ChangeScene());
+            }
         }
+        else if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
     }
     public IEnumerator ChangeScene()
     {
